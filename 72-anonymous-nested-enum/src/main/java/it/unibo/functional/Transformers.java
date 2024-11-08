c6bb5fca3315775166edb5ebd274b7016ca236a5
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2024-11-08 14:58:03 +0100

    Finished exercise 72

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,14 @@ public final class Transformers {
      * @param <O> output elements type
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        return flattenTransform(base, new Function<I, List<O>>() {
+
+            @Override
+            public List<O> call(final I input) {
+                return List.of(transformer.call(input));
+            }
+
+        });
     }
 
     /**
@@ -70,7 +77,7 @@ public final class Transformers {
      * @param <I> type of the collection elements
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
@@ -87,7 +94,18 @@ public final class Transformers {
      * @param <I> elements type
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return flattenTransform(base, new Function<I,List<I>>() {
+
+            @Override
+            public List<I> call(final I input) {
+                if (test.call(input)) {
+                    return List.of(input);
+                } else {
+                    return List.of();
+                }
+            }
+            
+        });
     }
 
     /**
@@ -103,6 +121,13 @@ public final class Transformers {
      * @param <I> elements type
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return select(base, new Function<I,Boolean>() {
+
+            @Override
+            public Boolean call(final I input) {
+                return !test.call(input);
+            }
+            
+        });
     }
 }
--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,21 +1,106 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
-import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+    
+    /**
+     * Costants representing the number of days in a month.
+     */
+    public static final int MONTH_31 = 31;
+    public static final int MONTH_30 = 30;
+    public static final int MONTH_28 = 28;
+
+    /**
+     * Enumeration representing the months in a non-leap year.
+     */
+    public enum Month {
+        GENUARY(MONTH_31), FEBRUARY(MONTH_28), MARCH(MONTH_31),
+        APRIL(MONTH_30), MAY(MONTH_31), JUNE(MONTH_30),
+        JULY(MONTH_31), AUGUST(MONTH_31), SEPTEMBER(MONTH_30),
+        OCTOBER(MONTH_31), NOVEMBER(MONTH_30), DECEMBER(MONTH_31);
+
+        private final int days;
+
+        /**
+         * Messages used for the exceptions.
+         */
+        public static final String MSG_GREATER = "Too many months found";
+        public static final String MSG_ZERO = "No months found";
+
+        /**
+         * Construct a Month enum with the specified number of days.
+         * 
+         * @param days The number of days in a month.
+         */
+        private Month(final int days) {
+            this.days = days;
+        }
+
+        /**
+         * Given an input string, returns the month which best represents it.
+         * Throws an exception if the input string matches with no months or more than one month. 
+         * 
+         * @param inputMonth The input string.
+         * @return The month found.
+         * @throws IllegalArgumentException if the input string matches with no months or more than one month. 
+         */
+        public static Month fromString(final String inputMonth) {
+            Month searchMonth = null;
+            int numberOfMonthFound = 0;
+    
+            for (final var elem : values()) {
+                if (elem.name().startsWith(inputMonth.toUpperCase())) {
+                    searchMonth = elem;
+                    numberOfMonthFound++;
+                }
+            }
+    
+            if (numberOfMonthFound > 1) {
+                throw new java.lang.IllegalArgumentException(MSG_GREATER);
+            }
+            if (numberOfMonthFound == 0) {
+                throw new java.lang.IllegalArgumentException(MSG_ZERO);
+            }
+
+            return searchMonth;
+        }
+    }
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
+    }
+
+    /**
+     * Comparator static nested class which sort months by the number of days.
+     */
+    public static class SortByDate implements Comparator<String> {
+        
+        @Override
+        public int compare(String o1, String o2) {
+            return Integer.compare(Month.fromString(o1).days, Month.fromString(o2).days);
+        }
+        
+    }
+
+    /**
+     * Comparator static nested class which sort months by the order in the calendar.
+     */
+    public static class SortByMonthOrder implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            return Month.fromString(o1).compareTo(Month.fromString(o2));
+        }
+        
     }
 }
